b02a502897054a7eb466dd24065f5eb133376153
Didier Spaier <didier@slint.fr>
2022-01-04 01:07:25 +0100

    Update of README_TESTING and fix of translate.sh

--- HandBook/translate.sh
+++ HandBook/translate.sh
@@ -1,1 +1,1 @@
-po4a-translate -f asciidoc -o compat=asciidoctor -m HandBook.pot -M UTF-8 -l $l.HandBookOutput.adoc -p $l.HandBook.po
+po4a-translate -f asciidoc -o compat=asciidoctor -m HandBook.pot -M UTF-8 -l $l.HandBookOutput.adoc -p $l.HandBook.2.adoc
